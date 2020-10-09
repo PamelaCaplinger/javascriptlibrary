1d12fcd174359673943bbde95b3a7f3df9e69737
PamelaCaplinger <caplinger_pamela@yahoo.com>
2020-10-09 10:33:02 -0400

    firstcommit

---
+++ 03-types.js
@@ -0,0 +1,104 @@
+//* BOOLEANS
+ let on= true;
+ console.log(on);
+ let off = false;
+ console.log(off);
+
+ //* NULL
+ let empty=null;
+ console.log(empty);
+
+ //* UNDEFINED
+ let undef= undefined;
+ console.log(undef)
+
+    
+ let x;
+ console.log(x);
+
+ //* NUMBERS
+
+ let degrees =90;
+ console.log(degrees)
+
+
+
+
+
+ let precise=999999999999999
+ console.log(precise);
+
+ let rounded= 9999999999999999;
+ console.log(rounded);
+
+
+ let notQuite= 0.2 === 0.1;
+ console.log(notQuite)
+
+ //* STRINGS
+ let stringOne="double quotes";
+ let stringtwo='single quotes';
+ let stringThree= "I/'m home";
+ let stringFour= `I'm home`;
+ let stringFive= `You can use ${"stringOne"} or ${"stringTwo"}!`
+
+ console.log(stringFive)
+
+ let first= 1050+100;
+ let second= '1050' + '100';
+
+ console.log(first);
+ console.log(second);
+
+ let animal='dog';
+
+ console.log('I have a '+ animal);
+ console.log(`I have a ${animal}`);
+
+ //
+
+ 
+ //* OBJECTS
+
+ let frodo= {
+     race:'hobbit',
+     rings: 1,
+     isShort: true
+ }
+
+console.log(frodo);
+console.log(typeof frodo);
+
+//* ARRAYS
+let tacos= ['large', 4, true];
+console.log(tacos);
+console.log(typeof tacos)
+
+let  firstName= "Pam";
+let  lastName= "Johnson";
+let number= 1107;
+let streetName= "Erie"; 
+let city= "Lapel";
+let state= "IN";
+let zipcode= 46051;
+
+console.log`${firstName}${lastName} ${houseNumber} ${street}, ${city} ${state} ${zipcode}`
+consold.log(address);
+
+
+//* STRING PROPERTIES AND METHODS
+let myName= 'Danielle';
+console.log(name.length)
+
+//* Methods
+
+console.log(Myname)
+console.log(myName.toUppercase());
+
+let home = 'My home is Fishers';
+console.log(home.includes('Fishers'));
+
+
+
+
+
